49f9350e4a43bdb102d041a213df52d927ba5046
Viktor Papara <eucalyptov@gmail.com>
2019-10-19 21:51:56 +0200

    Make bite pass test_dedup_and_title_case_names()

--- 5/names.py
+++ 5/names.py
@@ -7,7 +7,16 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
        each name appears only once"""
-    pass
+    output = []
+    for name in names:
+        firstname, lastname = name.split()
+        name = firstname.capitalize() + " " + lastname.capitalize()
+        if name not in output:
+            output.append(name)
+        else:
+            continue
+
+    return output
 
 
 def sort_by_surname_desc(names):
@@ -22,3 +31,7 @@ def shortest_first_name(names):
     """
     names = dedup_and_title_case_names(names)
     # ...
+
+
+if __name__ == '__main__':
+    dedup_and_title_case_names(NAMES)
