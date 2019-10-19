ee05d006ddc99c92eff86b89f35446f247561e03
Viktor Papara <eucalyptov@gmail.com>
2019-10-19 15:42:29 +0200

    Sumbit bite 104 @ codechalleng.es

--- 104/pipe.py
+++ 104/pipe.py
@@ -4,7 +4,14 @@ Have fun with our Bites of Py.
 Keep calm and code in Python!
 Become a PyBites ninja!"""
 
+
 def split_in_columns(message=message):
     """Split the message by newline (\n) and join it together on '|'
        (pipe), return the obtained output string"""
-    pass
+    lines = message.split(sep="\n")
+    output = "|".join(lines)
+    return output
+
+
+if __name__ == '__main__':
+    print(split_in_columns())
