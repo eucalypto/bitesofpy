0bba965b672ac908b72c000217e16c38cea7a3ee
Viktor Papara <eucalyptov@gmail.com>
2019-10-21 14:19:51 +0200

    Submit bite 8

--- 8/rotate.py
+++ 8/rotate.py
@@ -2,4 +2,11 @@ def rotate(string, n):
     """Rotate characters in a string.
        Expects string and n (int) for number of characters to move.
     """
-    pass
+    output = ""
+    output += string[n:]
+    output += string[:n]
+    return output
+
+if __name__ == '__main__':
+    print(rotate("abcde", 1))
+    print(rotate("abcde", -1))
