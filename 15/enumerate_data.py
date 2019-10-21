0dd9f97c771a02d6b5a7895f12a39b7ae34efbd5
Viktor Papara <eucalyptov@gmail.com>
2019-10-21 15:12:35 +0200

    Submit bite 15

--- 15/enumerate_data.py
+++ 15/enumerate_data.py
@@ -1,3 +1,5 @@
+from typing import List
+
 names = 'Julian Bob PyBites Dante Martin Rodolfo'.split()
 countries = 'Australia Spain Global Argentina USA Mexico'.split()
 
@@ -10,4 +12,9 @@ def enumerate_names_countries():
        4. Dante      Argentina
        5. Martin     USA
        6. Rodolfo    Mexico"""
-    pass
+    for index, full_name in enumerate(zip(names, countries), start=1):
+        print(f"{index}. {full_name[0]:<10} {full_name[1]}")
+
+
+if __name__ == '__main__':
+    enumerate_names_countries()
