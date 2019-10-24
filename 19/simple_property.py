42a825839b97721be2fc17690610a87b6ee10325
Viktor Papara <eucalyptov@gmail.com>
2019-10-24 23:16:32 +0200

    Submit bite 19

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -1,7 +1,26 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 NOW = datetime.now()
 
 
 class Promo:
-    pass
+    def __init__(self, name: str, expires: datetime):
+        self.name = name
+        self.expires = expires
+
+    @property
+    def expired(self) -> bool:
+        """Get the expired status."""
+        if datetime.now() <= self.expires:
+            return False
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    promo = Promo("20% off until tomorrow", datetime.today() + timedelta(days=1))
+
+    print(promo.expired)
+    print(promo.expires)
+    promo.expires = datetime.now()
+    print(promo.expired)
