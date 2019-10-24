64c14584ad93999890d04c07997f71f8c91f74c9
Viktor Papara <eucalyptov@gmail.com>
2019-10-24 21:53:42 +0200

    Change function into generator

--- 16/gendates.py
+++ 16/gendates.py
@@ -4,15 +4,29 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    dates = []
     date = PYBITES_BORN
     next_anniversary = PYBITES_BORN.replace(year=PYBITES_BORN.year + 1)
-    for i in range(10):
+    for _ in range(100):
         date += timedelta(days=100)
         if date < next_anniversary:
-            dates.append(date)
+            yield date
         else:
-            dates.append(next_anniversary)
-            dates.append(date)
+            output = next_anniversary
+            date -= timedelta(days=100)  # roll back for next yield
             next_anniversary = next_anniversary.replace(year=next_anniversary.year + 1)
-    return dates
+            yield output
+
+if __name__ == '__main__':
+    dates = gen_special_pybites_dates()
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+    print(next(dates))
+
