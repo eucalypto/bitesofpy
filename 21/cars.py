d5ca06733e3cf71f5de47473f122993a6c4d882e
Viktor Papara <eucalyptov@gmail.com>
2019-10-25 19:45:56 +0200

    Submit bite 21
    I LOVE python (list) comprehensions!

--- 21/cars.py
+++ 21/cars.py
@@ -1,3 +1,6 @@
+import operator
+from functools import reduce
+
 cars = {
     'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
     'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
@@ -10,22 +13,34 @@ cars = {
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+    jeep_models = cars['Jeep']
+    return ", ".join(jeep_models)
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    return [models[0] for models in cars.values()]
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    return sorted([model for model
+                   in reduce(operator.add, cars.values())
+                   if grep.lower() in model.lower()
+                   ])
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    pass
+    return {manufacturer: sorted(models)
+            for manufacturer, models in cars.items()}
+
+
+if __name__ == '__main__':
+    print(get_all_jeeps())
+    print(get_first_model_each_manufacturer())
+    print(get_all_matching_models())
+    print(sort_car_models())
