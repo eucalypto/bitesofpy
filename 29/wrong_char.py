bbd64d08bfa9e44badeae20b3844f0c74f5a5117
Viktor Papara <eucalyptov@gmail.com>
2019-10-25 23:35:00 +0200

    Submit bite 29
    I don't like this solution. It is not easy to read and is unnecessarily
    complicated. But it makes the code pass the test. ^-^

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,26 @@
 def get_index_different_char(chars):
-    pass
+    chars = [str(char) for char in chars]
+    alphanumerics = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    count_of_alphanumerics = 0
+    count_of_non_alphanumerics = 0
+    for char in chars:
+        if char in alphanumerics:
+            count_of_alphanumerics += 1
+        else:
+            count_of_non_alphanumerics += 1
+    look_for_alphanumeric = None
+    if count_of_alphanumerics == 1:
+        look_for_alphanumeric = True
+    elif count_of_non_alphanumerics == 1:
+        look_for_alphanumeric = False
+    else:
+        message = f"This should never be reached. Maybe both categories have more than 1 entries? alphanumeric: {count_of_alphanumerics} non-alpha: {count_of_non_alphanumerics}"
+        raise AssertionError(message)
+
+    for index, char in enumerate(chars):
+        if look_for_alphanumeric == (char in alphanumerics):
+            return index
+
+
+if __name__ == '__main__':
+    print(get_index_different_char([",", "", ",", "8"]))
