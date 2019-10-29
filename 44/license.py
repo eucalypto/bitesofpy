86d01ee316ad8d6c432338341f082bff1e8499ab
Viktor Papara <eucalyptov@gmail.com>
2019-10-29 23:51:26 +0100

    Submit bite 44

--- 44/license.py
+++ 44/license.py
@@ -1,1 +1,17 @@
-def gen_key(...
+import string
+import secrets
+
+
+def gen_key(parts=4, chars_per_part=8):
+    alphabet = string.ascii_uppercase + string.digits
+    return '-'.join(
+        [''.join(
+            [secrets.choice(alphabet) for _ in range(chars_per_part)]
+        ) for _ in range(parts)
+        ]
+    )
+
+
+if __name__ == '__main__':
+    print(gen_key())
+    print(gen_key(parts=2, chars_per_part=3))
