27bbf1a0c0eac8fb288948ac77b07f8da550def8
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 21:02:25 +0100

    Submit bite 77

--- 77/uncommon.py
+++ 77/uncommon.py
@@ -1,4 +1,11 @@
+import itertools
+
+
 def uncommon_cities(my_cities, other_cities):
     """Compare my_cities and other_cities and return the number of different
        cities between the two"""
-    pass
+
+    not_common = [city
+                  for city in (*my_cities, *other_cities)
+                  if not (city in my_cities and city in other_cities)]
+    return len(not_common)
