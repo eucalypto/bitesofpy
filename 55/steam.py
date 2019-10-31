9e002d59290a164d22b86ec65bf0bad6100c3d6c
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 13:56:04 +0100

    Submit bite 55

--- 55/steam.py
+++ 55/steam.py
@@ -10,4 +10,15 @@ Game = namedtuple('Game', 'title link')
 
 def get_games():
     """Parses Steam's RSS feed and returns a list of Game namedtuples"""
-    pass
+    parsed = feedparser.parse(FEED_URL)
+    items = parsed['items']
+    games = []
+    for item in items:
+        game = Game(title=item["title"],
+                    link=item['links'][0]['href'])
+        games.append(game)
+    return games
+
+
+if __name__ == '__main__':
+    print(len(get_games()))
