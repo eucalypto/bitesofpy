1ff0aec4df43af5cae9603ef16d7b92a470c8407
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 18:32:36 +0100

    Submit bite 66

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,5 +1,20 @@
+import itertools
+import operator
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
-    pass
+    return (round(accumulated_sum / count, ndigits=2)
+            for accumulated_sum, count
+            in zip(itertools.accumulate(sequence),
+                   itertools.count(1)
+                   )
+            )
+
+
+if __name__ == '__main__':
+    print(list(running_mean([1, 2, 3])))
+    for i in running_mean([1, 2, 3]):
+        print(i)
