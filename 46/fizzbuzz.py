11c132938fb67acba4972296be6eaaeb8ea7bae2
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 12:27:20 +0100

    Submit bite 46

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,2 +1,10 @@
 def fizzbuzz(num):
-    pass
+    words = []
+    if num % 3 == 0:
+        words.append("Fizz")
+    if num % 5 == 0:
+        words.append("Buzz")
+    if words != []:
+        return " ".join(words)
+    else:
+        return num
