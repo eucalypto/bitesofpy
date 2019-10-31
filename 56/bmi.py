c58c8fa887ec685efc80f15260557f14ea025c4c
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 14:52:11 +0100

    Submit bite 56

--- 56/bmi.py
+++ 56/bmi.py
@@ -10,10 +10,12 @@ def calc_bmi(weight, length):
 
 
 def create_parser():
-    """TODO:
-       Create an ArgumentParser adding the right arguments to pass the tests,
-       returns a argparse.ArgumentParser object"""
-    pass
+    """Create an ArgumentParser adding the right arguments to pass the tests,
+       return a argparse.ArgumentParser object"""
+    parser = argparse.ArgumentParser(description="Calculate your BMI")
+    parser.add_argument("-w", "--weight", help="Your weight in kg")
+    parser.add_argument("-l", "--length", help="Your length in cm")
+    return parser
 
 
 def handle_args(args=None):
