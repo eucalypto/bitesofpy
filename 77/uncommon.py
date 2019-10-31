8a3387aa5806a1b9daf9786173062246ff0d6871
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 21:06:57 +0100

    Add pybites solution for bite 77

--- 77/uncommon.py
+++ 77/uncommon.py
@@ -9,3 +9,11 @@ def uncommon_cities(my_cities, other_cities):
                   for city in (*my_cities, *other_cities)
                   if not (city in my_cities and city in other_cities)]
     return len(not_common)
+
+
+# This is the pybites solution.
+# It uses sets and the short syntax for symmetric_difference()
+def uncommon_cities_from_pybites(my_cities, other_cities):
+    """Compare my_cities and other_cities and return the number of different
+       cities between the two"""
+    return len(set(my_cities) ^ set(other_cities))
