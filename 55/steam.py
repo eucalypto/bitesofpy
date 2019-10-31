01a2ebccfa1269a68c0221be32af15629222a699
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 14:14:13 +0100

    Use attributes directly and rename variables

--- 55/steam.py
+++ 55/steam.py
@@ -10,10 +10,10 @@ Game = namedtuple('Game', 'title link')
 
 def get_games():
     """Parses Steam's RSS feed and returns a list of Game namedtuples"""
-    parsed = feedparser.parse(FEED_URL)
-    return [Game(title=item["title"], link=item['links'][0]['href'])
-            for item in parsed.entries]
+    feed = feedparser.parse(FEED_URL)
+    return [Game(title=entry.title, link=entry.link)
+            for entry in feed.entries]
 
 
 if __name__ == '__main__':
-    print(len(get_games()))
+    print(get_games())
