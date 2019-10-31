f722dd71af682ab4fe3ca7384ba08c9a27c2efbd
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 13:07:06 +0100

    Submit bite 54

--- 54/poem.py
+++ 54/poem.py
@@ -1,5 +1,34 @@
+from collections import deque
+
 INDENTS = 4
 
 
-def print_hanging_indents(poem):
-    pass
+def print_hanging_indents(poem: str):
+    lines = poem.strip().split("\n")
+    lines = map(str.strip, lines)
+    formatted_poem = ""
+    is_first_line = True
+    indent = " " * INDENTS
+    for line in lines:
+        if line == "":
+            is_first_line = True
+            continue
+        if is_first_line:
+            formatted_poem += line + "\n"
+            is_first_line = False
+        else:
+            formatted_poem += indent + line + "\n"
+    print(formatted_poem)
+
+
+
+if __name__ == '__main__':
+    shakespeare_unformatted = """
+                              To be, or not to be, that is the question:
+                              Whether 'tis nobler in the mind to suffer
+
+                              The slings and arrows of outrageous fortune,
+                              Or to take Arms against a Sea of troubles,
+                              """
+
+    print_hanging_indents(shakespeare_unformatted)
