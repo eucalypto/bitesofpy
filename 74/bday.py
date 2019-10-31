5d140f9b40b32a24562e42b82b0923da296997e0
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 20:51:59 +0100

    Submit bite 74

--- 74/bday.py
+++ 74/bday.py
@@ -1,6 +1,12 @@
 import calendar
+import datetime
 
 
-def weekday_of_birth_date(date):
+def weekday_of_birth_date(date: datetime.date):
     """Takes a date object and returns the corresponding weekday string"""
-    pass
+    return calendar.day_name[date.weekday()]
+
+
+if __name__ == '__main__':
+    print(weekday_of_birth_date(datetime.date.today()))
+    print(weekday_of_birth_date(datetime.date(1988, 8, 14)))
