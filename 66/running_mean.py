a071a673f579d3fdf847d268d6bff8629a0e5c00
Viktor Papara <eucalyptov@gmail.com>
2019-10-31 18:42:21 +0100

    Add official solution to bite 66

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -14,6 +14,15 @@ def running_mean(sequence):
             )
 
 
+# As always, I find the solution from pybites more elegant and plain
+def running_mean_after_pybites(sequence):
+    """Calculate the running mean of the sequence passed in,
+       returns a sequence of same length with the averages.
+       You can assume all items in sequence are numeric."""
+    for count, accumulated_sum in enumerate(itertools.accumulate(sequence), start=1):
+        yield round(accumulated_sum / count, ndigits=2)
+
+
 if __name__ == '__main__':
     print(list(running_mean([1, 2, 3])))
     for i in running_mean([1, 2, 3]):
