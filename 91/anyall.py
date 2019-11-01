39dff47d0837d4e68a5f4788b20cbc41546ac649
Viktor Papara <eucalyptov@gmail.com>
2019-11-01 17:16:37 +0100

    Submit bite 91

--- 91/anyall.py
+++ 91/anyall.py
@@ -1,3 +1,5 @@
+import string
+
 VOWELS = 'aeiou'
 PYTHON = 'python'
 
@@ -5,16 +7,28 @@ PYTHON = 'python'
 def contains_only_vowels(input_str):
     """Receives input string and checks if all chars are
        VOWELS. Match is case insensitive."""
-    pass
+    for char in input_str.lower():
+        if char not in VOWELS:
+            return False
+    # else:
+    return True
 
 
 def contains_any_py_chars(input_str):
     """Receives input string and checks if any of the PYTHON
        chars are in it. Match is case insensitive."""
-    pass
+    for char in input_str.lower():
+        if char in PYTHON:
+            return True
+    # else:
+    return False
 
 
 def contains_digits(input_str):
     """Receives input string and checks if it contains
        one or more digits."""
-    pass
+    for char in input_str:
+        if char in string.digits:
+            return True
+    # else:
+    return False
