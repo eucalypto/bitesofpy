30abba4e91cbe3c07d3379974ca2cb6c6b56ca9f
Viktor Papara <eucalyptov@gmail.com>
2019-11-03 21:59:11 +0100

    Implement last two methods in bite 30
    So that all 3 methods pass the tests.

--- 30/directors.py
+++ 30/directors.py
@@ -42,16 +42,29 @@ def get_movies_by_director():
 def calc_mean_score(movies):
     """Helper method to calculate mean of list of Movie namedtuples,
        round the mean to 1 decimal place"""
-    pass
+    scores = [float(movie.score) for movie in movies]
+    average = sum(scores) / len(scores)
+    return round(average, ndigits=1)
 
 
-def get_average_scores(directors):
+def get_average_scores(directors: dict):
     """Iterate through the directors dict (returned by get_movies_by_director),
        return a list of tuples (director, average_score) ordered by highest
        score in descending order. Only take directors into account
        with >= MIN_MOVIES"""
-    pass
+    director_scores = [(director, calc_mean_score(movies))
+                 for director, movies in directors.items()
+                 if len(movies) >= MIN_MOVIES]
+    director_scores.sort(key=lambda entry: entry[1], reverse=True)
+    return director_scores
 
 
 if __name__ == '__main__':
-    print(get_movies_by_director())
+    movies = get_movies_by_director()
+    # print(movies)
+    nolan_movies = movies.get("Christopher Nolan")
+    print(nolan_movies)
+    print(calc_mean_score(nolan_movies))
+    average_scores = get_average_scores(movies)
+    print(average_scores)
+
