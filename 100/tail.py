8cf4c26a253a1c5e300f05910e9cd591dd137961
Viktor Papara <eucalyptov@gmail.com>
2019-11-03 20:55:20 +0100

    Submit bite 100

--- 100/tail.py
+++ 100/tail.py
@@ -1,4 +1,6 @@
 def tail(filepath, n):
     """Similate Unix' tail -n, read in filepath, parse it into a list,
        strip newlines and return a list of the last n lines"""
-    pass
+    with open(filepath) as file:
+        lines = [line.rstrip() for line in file.readlines()]
+    return lines[-n:]
