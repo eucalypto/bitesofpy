794aec8881493d81956ac42bf7c8a9aa244d22e5
Viktor Papara <eucalyptov@gmail.com>
2019-11-07 19:48:08 +0100

    Submit bite 115

--- 115/indents.py
+++ 115/indents.py
@@ -1,3 +1,19 @@
 def count_indents(text):
     """Takes a string and counts leading white spaces, return int count"""
-    pass
+    whitespace_count = 0
+    for char in text:
+        if char != " ":
+            break
+        whitespace_count += 1
+
+    return whitespace_count
+
+
+def test_count_indents():
+    print(count_indents(" hello world  "))
+    print(count_indents("    Gandalf the Grey"))
+
+
+if __name__ == '__main__':
+    test_count_indents()
+
