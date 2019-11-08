18f1b2ab873a548e38f0ab39abdf29d56a8848f3
Viktor Papara <eucalyptov@gmail.com>
2019-11-08 10:32:55 +0100

    Submit bite 7

--- 7/logtimes.py
+++ 7/logtimes.py
@@ -14,7 +14,7 @@ with open(logfile) as f:
 
 # for you to code:
 
-def convert_to_datetime(line):
+def convert_to_datetime(line: str):
     """TODO 1:
        Extract timestamp from logline and convert it to a datetime object.
        For example calling the function with:
@@ -22,13 +22,23 @@ def convert_to_datetime(line):
        returns:
        datetime(2014, 7, 3, 23, 27, 51)
     """
-    pass
+    time_string = line.split()[1]
+    return datetime.fromisoformat(time_string)
 
 
-def time_between_shutdowns(loglines):
+def time_between_shutdowns(loglines=loglines):
     """TODO 2:
        Extract shutdown events ("Shutdown initiated") from loglines and
        calculate the timedelta between the first and last one.
        Return this datetime.timedelta object.
     """
-    pass
+    shutdown_times = [convert_to_datetime(line)
+                      for line in loglines
+                      if "Shutdown initiated." in line]
+
+    start, stop = shutdown_times[:2]
+    return stop - start
+
+if __name__ == '__main__':
+    print(convert_to_datetime("INFO 2014-07-03T23:27:51 supybot Shutdown complete."))
+    print(time_between_shutdowns())
