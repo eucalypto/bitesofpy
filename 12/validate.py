8148c6a9829f022b855be52ac5acb309d2116ce5
Viktor Papara <eucalyptov@gmail.com>
2019-11-10 17:13:15 +0100

    Submit bite 12

--- 12/validate.py
+++ 12/validate.py
@@ -11,5 +11,33 @@ USERS = (julian, bob, pybites)
 
 # define exception classes here
 
-def get_secret_token(username):
+
+class UserAccessExpired(Exception):
     pass
+
+
+class UserNoPermission(Exception):
+    pass
+
+
+class UserDoesNotExist(Exception):
+    pass
+
+
+def get_secret_token(username):
+    if username not in [user.name for user in USERS]:
+        raise UserDoesNotExist
+
+    this_user = None
+    for user in USERS:
+        if user.name == username:
+            this_user = user
+            break
+
+    if this_user.expired:
+        raise UserAccessExpired
+
+    if not this_user.role == ADMIN:
+        raise UserNoPermission
+
+    return SECRET
