42bfc39fe5dee266d3967e7f127af685d3205d25
Viktor Papara <eucalyptov@gmail.com>
2019-11-11 11:49:49 +0100

    Submit bite 13

--- 13/dict_convert.py
+++ 13/dict_convert.py
@@ -2,7 +2,6 @@ from collections import namedtuple
 from datetime import datetime
 import json
 
-
 blog = dict(name='PyBites',
             founders=('Julian', 'Bob'),
             started=datetime(year=2016, month=12, day=19),
@@ -12,9 +11,22 @@ blog = dict(name='PyBites',
 
 # define namedtuple here
 
+blog_nt = namedtuple("Blog", "name, founders, started, tags, location, site")
+
+
 def dict2nt(dict_):
-    pass
+    # return blog_nt._make(blog.values())
+    return blog_nt(**blog)
 
 
-def nt2json(nt):
-    pass
+def nt2json(nt: blog_nt):
+    # Without the following default=str, you get the error:
+    #   TypeError: Object of type datetime is not JSON serializable
+    # default takes a default conversion function for objects that
+    # json can't convert itself. Here, I just used the string
+    # conversion function.
+    return json.dumps(nt._asdict(), default=str)
+
+
+if __name__ == '__main__':
+    print(nt2json(dict2nt(blog)))
