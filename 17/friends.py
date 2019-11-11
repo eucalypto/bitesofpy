ae9b0273a67fe73ccf79b16f7877694a33462971
Viktor Papara <eucalyptov@gmail.com>
2019-11-11 18:46:40 +0100

    Improve bite 17 with other's solutions

--- 17/friends.py
+++ 17/friends.py
@@ -2,10 +2,10 @@ import itertools
 
 
 def friends_teams(friends, team_size=2, order_does_matter=False):
-    if order_does_matter:
-        return itertools.permutations(friends, team_size)
-
-    return itertools.combinations(friends, team_size)
+    func = itertools.combinations \
+        if not order_does_matter \
+        else itertools.permutations
+    return func(friends, team_size)
 
 
 if __name__ == '__main__':
