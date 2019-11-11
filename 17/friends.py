a1d132e60dbee92b147bc16669b8766044b5e29c
Viktor Papara <eucalyptov@gmail.com>
2019-11-11 18:24:10 +0100

    Submit bite 17

--- 17/friends.py
+++ 17/friends.py
@@ -1,2 +1,18 @@
-def friends_teams():
-    pass
+import itertools
+
+
+def friends_teams(friends, team_size=2, order_does_matter=False):
+    if order_does_matter:
+        return itertools.permutations(friends, team_size)
+
+    return itertools.combinations(friends, team_size)
+
+
+if __name__ == '__main__':
+    friends = "Gandalf, Gimli, Aragorn, Legolas".split(", ")
+    for team in friends_teams(friends):
+        print(team)
+
+    print("Order does matter:")
+    for team in friends_teams(friends, order_does_matter=True):
+        print(team)
