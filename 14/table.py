60ac5ff06d601f5d17dba09559713f96f7fbecbb
Viktor Papara <eucalyptov@gmail.com>
2019-11-11 17:45:13 +0100

    Submit bite 14

--- 14/table.py
+++ 14/table.py
@@ -7,5 +7,13 @@ awake = [True, False] * 3
 SEPARATOR = ' | '
 
 
-def generate_table():
-    pass
+def generate_table(*sequences):
+    rows = []
+    for row_items in zip(*sequences):
+        row_items_as_strings = map(str, row_items)
+        rows.append(SEPARATOR.join(row_items_as_strings))
+    return rows
+
+
+if __name__ == '__main__':
+    print(generate_table(names, aliases, points, awake))
