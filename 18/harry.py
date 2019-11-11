36641cf549e208149f9dc2137f367e19bce20f6f
Viktor Papara <eucalyptov@gmail.com>
2019-11-11 21:40:56 +0100

    Refine bite 18 solution
    Extract code into helper functions, use appropriate collections, and
    make word sorting process more clear.
    
    I like this version much more. The code is more clear and more
    expressive.

--- 18/harry.py
+++ 18/harry.py
@@ -1,7 +1,6 @@
 import os
 import urllib.request
-import string
-from collections import Counter
+from collections import Counter, deque
 
 # data provided
 stopwords_file = os.path.join('/tmp', 'stopwords')
@@ -11,32 +10,43 @@ urllib.request.urlretrieve('http://bit.ly/2C6RzuR', harry_text)
 
 
 def get_harry_most_common_word():
-    with open(stopwords_file) as file:
-        stopwords = [word.strip()
-                     for word in file.readlines()]
-
-    with open(harry_text) as file:
-        lines = file.readlines()
-
-    lines = [line.strip().lower() for line in lines]
-    words = [strip_non_alphanumeric(word)
-             for line in lines
-             for word in line.split()
-             if word not in stopwords]
-    words = [word
-             for word in words
-             if word != ""]
+    # Convert: deque(list) -> set to profit from hashed O(1) lookup
+    stopwords = set(get_words(stopwords_file))
+    words_non_filtered = get_words(harry_text)
+    words = deque()
+    for word in words_non_filtered:
+        word = word.lower()
+        word = strip_non_alphanumeric(word)
+        # add empty string to filtered words, so we don't have to filter
+        # it separately.
+        stopwords.add("")
+        if word in stopwords:
+            continue
+        words.append(word)
     count = Counter(words)
     return count.most_common()[0]
 
 
-def strip_non_alphanumeric(text: str):
-    return "".join([char
-                    for char in text
-                    if char in (string.ascii_lowercase + string.digits)])
+def strip_non_alphanumeric(text: str) -> str:
+    return "".join(char
+                   for char in text
+                   if char.isalnum())
 
 
-if __name__ == '__main__':
+def get_words(filename) -> deque:
+    """
+    Return list of words in the file with the name "filename"
+    """
+    words = deque()
+    with open(filename) as file:
+        for line in file.readlines():
+            for word in line.split():
+                words.append(word)
+    return words
+
+
+def test():
+    # print(get_words(stopwords_file))
+    # print(get_words(harry_text))
     print(strip_non_alphanumeric("2234 uiaern ;.iaren ;j uiae"))
-
     print(get_harry_most_common_word())
