e11f674af95af966329101dd95b1950225efb68f
Viktor Papara <eucalyptov@gmail.com>
2019-11-12 23:20:08 +0100

    Improve solution for bite 22
    Add functools.wraps() wrap to preserve identity of wrapped function.

--- 22/decorators.py
+++ 22/decorators.py
@@ -3,18 +3,34 @@ from functools import wraps
 
 def make_html(element):
     def decorator(func):
-        def make_html_wrap(*args, **kwargs):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            """wrapper function"""
             return f"<{element}>" \
                    + func(*args, **kwargs) \
                 + f"</{element}>"
-        return make_html_wrap
+        return wrap
     return decorator
 
 
-@make_html("p")
-def get_foo(text):
-    return text
-
-
 if __name__ == '__main__':
+    @make_html("p")
+    def get_foo(text):
+        """get_foo function"""
+        return text
+
+    def return_hello():
+        return "hello"
+
+    @make_html("bacon")
+    def return_nothing():
+        pass
+
     print(get_foo("hello"))
+    return_hello = make_html("div")(return_hello)
+    print(return_hello())
+    print(get_foo)
+    print(get_foo.__name__)
+    print(help(get_foo))
+
+    print(return_nothing())
