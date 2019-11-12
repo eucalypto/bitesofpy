207c822336c9e3b7be092372b6f40b3e49fe6ab5
Viktor Papara <eucalyptov@gmail.com>
2019-11-12 22:53:21 +0100

    Implement crude solution to bite 22

--- 22/decorators.py
+++ 22/decorators.py
@@ -2,4 +2,19 @@ from functools import wraps
 
 
 def make_html(element):
-    pass
+    def decorator(func):
+        def make_html_wrap(*args, **kwargs):
+            return f"<{element}>" \
+                   + func(*args, **kwargs) \
+                + f"</{element}>"
+        return make_html_wrap
+    return decorator
+
+
+@make_html("p")
+def get_foo(text):
+    return text
+
+
+if __name__ == '__main__':
+    print(get_foo("hello"))
