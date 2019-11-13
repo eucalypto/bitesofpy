c80a2193053f7b751ff26c595c790dcba667ffc0
Viktor Papara <eucalyptov@gmail.com>
2019-11-14 00:17:07 +0100

    Save work of today.
    The first function passes the test already. But the others are not even
    finished. :)

--- 27/omdb.py
+++ 27/omdb.py
@@ -19,16 +19,34 @@ files = glob.glob(os.path.join(TMP, '*json'))
 
 
 def get_movie_data(files=files):
-    pass
+    movies = []
+    for file_name in files:
+        with open(file_name) as file:
+            load = json.load(file)
+        movies.append(load)
 
+    return movies
 
 def get_single_comedy(movies):
-    pass
+    for movie in movies:
+        if "Comedy" in movie["Genre"]:
+            return movie["Title"]
 
 
 def get_movie_most_nominations(movies):
-    pass
+    most_nominations = 0
+    movie = None
+    for movie in movies:
+        awards = movie["Awards"]
+    
+
 
 
 def get_movie_longest_runtime(movies):
     pass
+
+
+if __name__ == '__main__':
+    movies = get_movie_data()
+    # print(get_single_comedy(movies))
+    get_movie_most_nominations(movies)
