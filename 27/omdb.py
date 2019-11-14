756d7f96713c4b2c53282b120ae4448bb6cb647b
Viktor Papara <eucalyptov@gmail.com>
2019-11-14 12:16:12 +0100

    Implement test-passing implementation of bite 27
    With helper function get_runtime() and its test!

--- 27/omdb.py
+++ 27/omdb.py
@@ -2,6 +2,7 @@ import glob
 import json
 import os
 from urllib.request import urlretrieve
+import re
 
 BASE_URL = 'http://projects.bobbelderbos.com/pcc/omdb/'
 MOVIES = ('bladerunner2049 fightclub glengary '
@@ -27,6 +28,7 @@ def get_movie_data(files=files):
 
     return movies
 
+
 def get_single_comedy(movies):
     for movie in movies:
         if "Comedy" in movie["Genre"]:
@@ -34,19 +36,36 @@ def get_single_comedy(movies):
 
 
 def get_movie_most_nominations(movies):
-    most_nominations = 0
-    movie = None
-    for movie in movies:
-        awards = movie["Awards"]
-    
-
+    # TODO: rewrite with max(movies, key=nominations)
+    movie_index = -1
+    movie_nominations = -1
+    for index, movie in enumerate(movies):
+        awards_str = movie["Awards"]
+        nominations = int(re.findall(r"([0-9]*) nominations\.$",
+                                     awards_str)[0])
+        if nominations > movie_nominations:
+            movie_index = index
+            movie_nominations = nominations
+    return movies[movie_index]["Title"]
 
 
 def get_movie_longest_runtime(movies):
-    pass
+    longest = max(movies, key=get_runtime)
+    return longest["Title"]
+
+
+def get_runtime(movie: dict) -> int:
+    runtime_str = movie["Runtime"]
+    return int(
+        re.findall(r"([0-9]*) min", runtime_str)[0]
+    )
 
 
 if __name__ == '__main__':
     movies = get_movie_data()
+    # print(movies)
+    # for movie in movies:
+    #     print(movie["Title"], movie["Runtime"])
     # print(get_single_comedy(movies))
-    get_movie_most_nominations(movies)
+    # print(get_movie_most_nominations(movies))
+    print(get_movie_longest_runtime(movies))
--- 27/test_omdb.py
+++ 27/test_omdb.py
@@ -1,5 +1,7 @@
+import pytest
 from omdb import (get_movie_data, get_single_comedy,
-                  get_movie_most_nominations, get_movie_longest_runtime)
+                  get_movie_most_nominations, get_movie_longest_runtime,
+                  get_runtime)
 
 movies = get_movie_data()
 
@@ -13,3 +15,12 @@ def test_data_analysis():
     assert get_single_comedy(movies) == 'Horrible Bosses'
     assert get_movie_most_nominations(movies) == 'Fight Club'
     assert get_movie_longest_runtime(movies) == 'Blade Runner 2049'
+
+
+@pytest.mark.parametrize(
+    "movie, expected_runtime",
+    zip(movies, [98, 107, 139, 164, 100])
+    # [(movie[0], 98), (movie[1], 107), ...]
+)
+def test_get_runtime(movie, expected_runtime):
+    assert get_runtime(movie) == expected_runtime
