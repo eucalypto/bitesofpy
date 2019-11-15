36b521cde83c1b1f087f81d7188ae8767ea2a42f
Viktor Papara <eucalyptov@gmail.com>
2019-11-15 10:11:10 +0100

    Implement very brute-force solution to bite 33
    But it does pass the tests! And it uses list comprehensions. ^-^

--- 33/transpose.py
+++ 33/transpose.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+
 def transpose(data):
     """Transpose a data structure
     1. dict
@@ -13,4 +16,32 @@ def transpose(data):
     In: transpose(data)
     Out: [('Bob', 'Julian'), (60, 221), (60, 34), (56, 78)]
     """
-    pass
+    if type(data) is dict:
+        months = (month for month, count in data.items())
+        counts = (count for month, count in data.items())
+        return [months, counts]
+    elif type(data) is list:
+        return [[member[index] for member in data]
+                for index in range(len(data[0]))]
+
+
+def testing():
+    mydict = {'2017-8': 19, '2017-9': 13}
+    enumerate(mydict)
+
+    Member = namedtuple("Member", "name, since, karma, bitecoins")
+    gandalf = Member("Gandalf", "1319-03", 13_332, 1000)
+    print(gandalf)
+    print(gandalf._fields)
+    print(gandalf[1])
+    print(len(gandalf))
+    # print(enumerate(gandalf))
+
+    print(transpose([gandalf]))
+
+    for item in transpose([gandalf]):
+        for data in item:
+            print(data)
+
+if __name__ == '__main__':
+    testing()
