2043537e5355ef200b31c4eddb37af5d72b190b4
Viktor Papara <eucalyptov@gmail.com>
2019-11-17 18:39:11 +0100

    Submit bite 36

--- 36/profile.py
+++ 36/profile.py
@@ -1,2 +1,17 @@
-def get_profile():
-    pass
+def get_profile(name, age, *sports, **awards):
+    """Returns profile as dict and makes checks beforehand."""
+    # Checks:
+    if not isinstance(age, int):
+        raise ValueError("Age is not an int")
+    if len(sports) > 5:
+        raise ValueError("Please give no more than 5 sports")
+
+    profile = {'name': name,
+               'age': age}
+    if sports:  # if sports not empty
+        profile['sports'] = list(sorted(sports))
+
+    if awards:  # if awards not empty
+        profile['awards'] = awards
+
+    return profile
