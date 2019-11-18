0f68c1221e18cf656c3cecb55ce508076bc57ce2
Viktor Papara <eucalyptov@gmail.com>
2019-11-18 12:59:16 +0100

    Submit bite 11

--- 11/account.py
+++ 11/account.py
@@ -10,3 +10,35 @@ class Account:
         return self.start_balance + sum(self._transactions)
 
     # add dunder methods below
+
+    def __str__(self):
+        return f"{self.name} account - balance: {self.balance}"
+
+    def __add__(self, amount):
+        self._transactions.append(int(amount))
+
+    def __sub__(self, amount):
+        self._transactions.append(-int(amount))
+
+    # Comparison operators: <, >, <=, >=, ==
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __le__(self, other):
+        return self.balance <= other.balance
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, item):
+        return self._transactions[item]
