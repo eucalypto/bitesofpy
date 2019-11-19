5dd0372796c1663b74ea2b826010bd232c131a8e
Viktor Papara <eucalyptov@gmail.com>
2019-11-19 14:16:57 +0100

    Submit bite 41

--- 41/login.py
+++ 41/login.py
@@ -1,12 +1,23 @@
+from functools import wraps
+
 known_users = ['bob', 'julian', 'mike', 'carmen', 'sue']
 loggedin_users = ['mike', 'sue']
 
 
 def login_required(func):
-    pass
+
+    @wraps(func)
+    def wrapped(user):
+        if user not in known_users:
+            return "please create an account"
+        if user not in loggedin_users:
+            return "please login"
+        return func(user)
+
+    return wrapped
 
 
 @login_required
 def welcome(user):
-    '''Return a welcome message if logged in'''
-    pass
+    """Return a welcome message if logged in"""
+    return f"welcome back {user}"
