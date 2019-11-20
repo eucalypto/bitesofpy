6e028b4c4d75c823f09a02b4fe9fca78fcb10c56
Viktor Papara <eucalyptov@gmail.com>
2019-11-20 01:08:33 +0100

    Submit bite 239
    The first 20 cases of fizzbuzz should be enough

--- 239/test_fizzbuzz.py
+++ 239/test_fizzbuzz.py
@@ -1,3 +1,17 @@
 from fizzbuzz import fizzbuzz
 
+import pytest
+
+
 # write one or more pytest functions below, they need to start with test_
+
+@pytest.mark.parametrize(
+    "input, expected",
+    list(enumerate([1, 2, 'Fizz', 4, 'Buzz', "Fizz", 7, 8,
+                    "Fizz" , "Buzz", 11, "Fizz", 13, 14,
+                    "Fizz Buzz", 16, 17, "Fizz", 19,
+                    "Buzz"],
+                   start=1))
+)
+def test_fizzbuzz_first_20(input, expected):
+    assert fizzbuzz(input) == expected
