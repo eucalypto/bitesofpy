e2b7f26188dc5140e7bf858da1085d0f6481cec4
Viktor Papara <eucalyptov@gmail.com>
2019-11-21 20:34:57 +0100

    Submit bite 47

--- 47/password.py
+++ 47/password.py
@@ -6,4 +6,38 @@ used_passwords = set('PassWord@1 PyBit$s9'.split())
 
 
 def validate_password(password):
-    pass
+    if len(password) < 6:
+        return False
+    if len(password) > 12:
+        return False
+
+    digits = 0
+    lower = 0
+    upper = 0
+    punctuation = 0
+    for char in password:
+        if char in string.digits:
+            digits += 1
+        elif char in string.ascii_lowercase:
+            lower += 1
+        elif char in string.ascii_uppercase:
+            upper += 1
+        elif char in PUNCTUATION_CHARS:
+            punctuation += 1
+    if digits < 1:
+        return False
+    if lower < 2:
+        return False
+    if upper < 1:
+        return False
+    if punctuation < 1:
+        return False
+    if password in used_passwords:
+        return False
+
+    used_passwords.add(password)
+    return True
+
+
+
+
