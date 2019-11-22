be00c30bb73db34f3f6e049deb79e01342673403
Viktor Papara <eucalyptov@gmail.com>
2019-11-22 19:29:53 +0100

    Submit Bite 117. Round a number even (a.k.a. banker's rounding)

--- 117/round_even.py
+++ 117/round_even.py
@@ -1,3 +1,14 @@
 def round_even(number):
     """Takes a number and returns it rounded even"""
-    pass
+    integer_part = int(number)
+    decimal_part = number - integer_part
+    if integer_part % 2 == 0:
+        if decimal_part <= 0.5:
+            return integer_part
+        else:
+            return integer_part + 1
+    else:
+        if decimal_part < 0.5:
+            return integer_part
+        else:
+            return integer_part + 1
