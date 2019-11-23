3f45108a77990f451abe29331d30ad6fb31742de
Viktor Papara <eucalyptov@gmail.com>
2019-11-23 12:14:08 +0100

    Implement crude solution to Bite 48: Make a bar chart of new Safari books

--- 48/safari.py
+++ 48/safari.py
@@ -3,8 +3,28 @@ import urllib.request
 
 LOG = os.path.join('/tmp', 'safari.logs')
 PY_BOOK, OTHER_BOOK = '🐍', '.'
+
 urllib.request.urlretrieve('http://bit.ly/2BLsCYc', LOG)
 
 
 def create_chart():
-    pass
+    with open(LOG) as logfile:
+        slack_entries = {}
+
+        last_line = ""
+        for line in logfile:
+            if "sending to slack channel" in line:
+                day = last_line[:5]
+                if day not in slack_entries.keys():
+                    slack_entries[day] = PY_BOOK if "python" in last_line.lower() else OTHER_BOOK
+                else:
+                    slack_entries[day] += PY_BOOK if "python" in last_line.lower() else OTHER_BOOK
+
+            last_line = line
+
+        for day, books in slack_entries.items():
+            print(day, books)
+
+
+if __name__ == '__main__':
+    create_chart()
