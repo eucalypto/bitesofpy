2ca2f59d445576c870b4ab6f3360c9a79e24c160
Viktor Papara <eucalyptov@gmail.com>
2019-11-23 14:29:17 +0100

    Implement solution for Bite 20. Write a context manager

--- 20/account.py
+++ 20/account.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 class Account:
 
     def __init__(self):
@@ -15,3 +17,15 @@ class Account:
 
     # add 2 dunder methods here to turn this class 
     # into a 'rollback' context manager
+
+    # I knew already that you can define a function as a context manager using
+    # @contextlib.contextmanager decorator. Now I know that you also can define
+    # your own class as context manager.
+    # See https://docs.python.org/3.7/reference/datamodel.html#with-statement-context-managers
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        while self.balance < 0:
+            self._transactions.pop()
