ef8e8c9afd697278b977493f54111e29e92793a4
Viktor Papara <eucalyptov@gmail.com>
2019-11-24 16:51:24 +0100

    Implement crude solution for Bite 49. Scrape Packt's html with BeautifulSoup
    It's ugly but it passes the tests. :)

--- 49/packt.py
+++ 49/packt.py
@@ -10,4 +10,22 @@ Book = namedtuple('Book', 'title description image link')
 
 def get_book():
     """make a Soup object, parse the relevant html sections, and return a Book namedtuple"""
-    pass
+    page = Soup(CONTENT, features="html.parser")
+    title = page.find(class_="dotd-title")
+
+    description = title.next_sibling.next_sibling\
+        .next_sibling.next_sibling.text.strip()
+
+    # print(page.prettify())
+
+    image_obj = page.find(class_="imagecache-dotd_main_image")
+    image = image_obj["src"]
+
+    book = page.find(class_="dotd-main-book")
+    # print(book.prettify())
+    link = book.find("a")["href"]
+
+    return Book(title.text.strip(), description, image, link)
+
+if __name__ == '__main__':
+    print(get_book())
