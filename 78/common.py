ba28aad768bcf3eecfe574102215c0c393226bb7
Viktor Papara <eucalyptov@gmail.com>
2019-11-27 09:46:43 +0100

    Implement solution for Bite 78. Find programmers with common languages

--- 78/common.py
+++ 78/common.py
@@ -1,4 +1,14 @@
-def common_languages(programmers):
+from functools import reduce
+
+
+def common_languages(programmers: dict):
     """Receive a dict of keys -> names and values -> a sequence of
        of programming languages, return the common languages"""
-    pass
+    language_sets = (set(languages)
+                     for languages in programmers.values())
+
+    return reduce(set.intersection, language_sets)
+
+
+if __name__ == '__main__':
+    common_languages({1: "c", 2: "c"})
