df6ab03b2607321e2e8999a8c71d1c1a71f5bf6a
Viktor Papara <eucalyptov@gmail.com>
2019-11-29 11:05:11 +0100

    Implement solution for Bite 60. Create a deck of Uno cards

--- 60/uno.py
+++ 60/uno.py
@@ -9,4 +9,16 @@ def create_uno_deck():
     """Create a deck of 108 Uno cards.
        Return a list of UnoCard namedtuples
        (for cards w/o suit use None in the namedtuple)"""
-    pass
+    colored_names = ["0"] \
+        + 2 * "1, 2, 3, 4, 5, 6, 7, 8, 9, Skip, Reverse, Draw Two".split(sep=", ")
+    uncolored_names = 4 * "Wild, Wild Draw Four".split(sep=", ")
+
+    return [UnoCard(suit, name)
+            for suit in SUITS
+            for name in colored_names] \
+        + [UnoCard(None, name)
+            for name in uncolored_names]
+
+
+if __name__ == '__main__':
+    print(len(create_uno_deck()))
