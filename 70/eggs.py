aa732198d8528de993b5eed6e4c9495ea119e4e2
Viktor Papara <eucalyptov@gmail.com>
2019-12-01 15:31:19 +0100

    Implement solution for Bite 70. Create your own iterator

--- 70/eggs.py
+++ 70/eggs.py
@@ -4,4 +4,15 @@ COLORS = 'red blue green yellow brown purple'.split()
 
 
 class EggCreator:
-    pass
+    def __init__(self, max_eggs):
+        self.max_eggs = max_eggs
+        self.eggs_produced = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.eggs_produced >= self.max_eggs:
+            raise StopIteration
+        self.eggs_produced += 1
+        return f"{choice(COLORS)} egg"
