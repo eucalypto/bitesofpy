755bce51e154018bb03695626ebd0bec1dba21f4
Viktor Papara <eucalyptov@gmail.com>
2019-12-04 14:30:45 +0100

    Implement solution for Bite 71. Keep state in a class + make its instance callable

--- 71/record.py
+++ 71/record.py
@@ -1,3 +1,10 @@
 class RecordScore():
+    max_score: int = None
+
     """Class to track a game's maximum score"""
-    pass
+    def __call__(self, current_score, *args, **kwargs):
+        if self.max_score is None:
+            self.max_score = current_score
+        if current_score > self.max_score:
+            self.max_score = current_score
+        return self.max_score
