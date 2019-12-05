f5397b807d05285724497031ef436b75e924e5f3
Viktor Papara <eucalyptov@gmail.com>
2019-12-05 16:46:46 +0100

    Improve solution for Bite 72. Retrieve the right Ninja Belt based on score

--- 72/belt.py
+++ 72/belt.py
@@ -10,8 +10,10 @@ MIN_SCORE, MAX_SCORE = min(scores), max(scores)
 def get_belt(user_score):
     if user_score < MIN_SCORE:
         return None
-    reached_scores = list(filter(lambda score: score <= user_score, scores))
-    return belts[len(reached_scores)-1]
+    reached_belts = [level[1]
+                     for level in HONORS.items()
+                     if user_score >= level[0]]
+    return reached_belts[-1]
 
 
 if __name__ == '__main__':
