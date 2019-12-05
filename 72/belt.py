3c5fd844a801e88b84571d5783fbaca8b6c1fc52
Viktor Papara <eucalyptov@gmail.com>
2019-12-05 16:38:58 +0100

    Implement solution for Bite 72. Retrieve the right Ninja Belt based on score

--- 72/belt.py
+++ 72/belt.py
@@ -1,3 +1,4 @@
+import itertools
 from collections import OrderedDict
 
 scores = [10, 50, 100, 175, 250, 400, 600, 800, 1000]
@@ -7,4 +8,11 @@ MIN_SCORE, MAX_SCORE = min(scores), max(scores)
 
 
 def get_belt(user_score):
-    pass
+    if user_score < MIN_SCORE:
+        return None
+    reached_scores = list(filter(lambda score: score <= user_score, scores))
+    return belts[len(reached_scores)-1]
+
+
+if __name__ == '__main__':
+    print(get_belt(10))
