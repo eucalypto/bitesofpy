cf3ecb9a8e45fe04f21288f22307d4ebac439ad6
Viktor Papara <eucalyptov@gmail.com>
2019-12-07 18:23:01 +0100

    Implement solution for Bite 79. Parse a csv file and create a bar chart

--- 79/community.py
+++ 79/community.py
@@ -1,6 +1,7 @@
 import csv
-
 import requests
+
+from collections import Counter
 
 CSV_URL = 'https://bit.ly/2HiD2i8'
 
@@ -8,10 +9,18 @@ CSV_URL = 'https://bit.ly/2HiD2i8'
 def get_csv():
     """Use requests to download the csv and return the
        decoded content"""
-    pass
+    return requests.get(CSV_URL).text
 
 
-def create_user_bar_chart(content):
+def create_user_bar_chart(content=get_csv()):
     """Receives csv file (decoded) content and returns a table of timezones
        and their corresponding member counts in pluses (see Bite/tests)"""
-    pass
+    timezones = (entry.split(sep=",")[-1] for entry in content.splitlines()[1:])
+    counter = Counter(timezones)
+    for timezone, amount in counter.most_common():
+        print(timezone.ljust(20) + "| " + amount * "+")
+
+
+if __name__ == '__main__':
+    # get_csv()
+    create_user_bar_chart()
