946ba2f2c92b537216f12762503d1305be5af1b2
Viktor Papara <eucalyptov@gmail.com>
2019-12-09 20:38:27 +0100

    Implement solution for Bite 82. Define a Score Enum and customize it adding methods

--- 82/score.py
+++ 82/score.py
@@ -2,8 +2,16 @@ from enum import Enum
 
 THUMBS_UP = '👍'  # in case you go f-string ...
 
-# move these into an Enum:
-BEGINNER = 2
-INTERMEDIATE = 3
-ADVANCED = 4
-CHEATED = 1
+
+class Score(Enum):
+    BEGINNER = 2
+    INTERMEDIATE = 3
+    ADVANCED = 4
+    CHEATED = 1
+
+    def __str__(self):
+        return f"{self.name} => {self.value * THUMBS_UP}"
+
+    @classmethod
+    def average(cls):
+        return sum(member.value for member in Score) / len(Score)
