37f3671bb176893b12d8207be8556fb7856eb13a
Viktor Papara <eucalyptov@gmail.com>
2019-12-10 18:53:17 +0100

    Implement solution for Bite 84. Flatten lists recursively (Droste Bite)

--- 84/flatten.py
+++ 84/flatten.py
@@ -1,2 +1,8 @@
 def flatten(list_of_lists):
-    pass
+    return_list = []
+    for item in list_of_lists:
+        if isinstance(item, (list, tuple)):
+            return_list += flatten(item)
+        else:
+            return_list.append(item)
+    return return_list
