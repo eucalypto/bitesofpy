9ab62626fa4d14de49732bdade7b92b4be1e10d9
Viktor Papara <eucalyptov@gmail.com>
2019-12-11 20:47:35 +0100

    Implement solution for Bite 86. Create a RGB-to-Hex converter

--- 86/rgb2hex.py
+++ 86/rgb2hex.py
@@ -2,4 +2,7 @@ def rgb_to_hex(rgb):
     """Receives (r, g, b)  tuple, checks if each rgb int is within RGB
        boundaries (0, 255) and returns its converted hex, for example:
        Silver: input tuple = (192,192,192) -> output hex str = #C0C0C0"""
-    pass
+    for color_value in rgb:
+        if not 0 <= color_value <= 255:
+            raise ValueError
+    return f"#{rgb[0]:02X}{rgb[1]:02X}{rgb[2]:02X}"
