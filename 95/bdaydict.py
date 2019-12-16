cba66c97a31c75b1443777b8e9e05b0159b97854
Viktor Papara <eucalyptov@gmail.com>
2019-12-16 14:28:42 +0100

    Implement solution for Bite 95. Subclass the dict built-in

--- 95/bdaydict.py
+++ 95/bdaydict.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 MSG = 'Hey {}, there are more people with your birthday!'
 
 
@@ -8,5 +10,9 @@ class BirthdayDict(dict):
     def __init__(self, *args, **kwargs):
         self.update(*args, **kwargs)
 
-    def __setitem__(self, name, birthday):
-        pass
+    def __setitem__(self, name, birthday: date):
+        def without_year(date: date):
+            return date.strftime("%m %d")
+        if without_year(birthday) in [without_year(birthday_) for birthday_ in self.values()]:
+            print(MSG.format(name))
+        dict.__setitem__(self, name, birthday)
