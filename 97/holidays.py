2a4398b6a975c089c8f1f7533fa2b8717bc61489
Viktor Papara <eucalyptov@gmail.com>
2019-12-17 13:45:36 +0100

    Implement solution for Bite 97. BeautifulSoup II - scrape US holidays

--- 97/holidays.py
+++ 97/holidays.py
@@ -3,6 +3,7 @@ import os
 from urllib.request import urlretrieve
 
 from bs4 import BeautifulSoup
+import bs4
 
 
 # prep data
@@ -17,11 +18,28 @@ urlretrieve(
 with open(holidays_page) as f:
     content = f.read()
 
-holidays = defaultdict(list)
+
 
 
 def get_us_bank_holidays(content=content):
     """Receive scraped html output, make a BS object, parse the bank
        holiday table (css class = list-table), and return a dict of
        keys -> months and values -> list of bank holidays"""
-    pass
+    holidays = defaultdict(list)
+    soup = BeautifulSoup(content, 'html.parser')
+    table = soup.find(class_="list-table").tbody
+    for entry in table.children:
+        # I hate this: table.children gives both Tag and NavigableString objects, so I have to manually
+        # ignore the latter
+        if isinstance(entry, bs4.element.NavigableString):
+            continue
+        name = entry.a.text.strip()
+        month = entry.time['datetime'].split("-")[1]
+        if month in holidays:
+            holidays[month].append(name)
+        else:
+            holidays[month] = [name]
+    return holidays
+
+if __name__ == '__main__':
+    get_us_bank_holidays()
