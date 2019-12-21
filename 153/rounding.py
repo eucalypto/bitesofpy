687e5f2a6b89887dbcd5b4e34946751bf37ef565
Viktor Papara <eucalyptov@gmail.com>
2019-12-21 19:15:22 +0100

    Implement solution for Bite 153. Round a sequence of numbers

--- 153/rounding.py
+++ 153/rounding.py
@@ -3,4 +3,5 @@ def round_up_or_down(transactions, up=True):
        If up=True (default) ronud up, else round down.
        Return a new list of rounded values
     """
-    pass
+    return [int(item) + 1 if up else int(item)
+            for item in transactions]
