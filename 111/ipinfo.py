7b4b1ea48e79b60927d585ff910397e7fc1dff1b
Viktor Papara <eucalyptov@gmail.com>
2019-12-21 19:05:58 +0100

    Implement solution for Bite 111. Use the ipinfo API to lookup IP country

--- 111/ipinfo.py
+++ 111/ipinfo.py
@@ -6,4 +6,9 @@ IPINFO_URL = 'http://ipinfo.io/{ip}/json'
 def get_ip_country(ip_address):
     """Receives ip address string, use IPINFO_URL to get geo data,
        parse the json response returning the country code of the IP"""
-    pass
+    response = requests.get(IPINFO_URL.format(ip=ip_address)).json()
+
+    return response['country']
+
+if __name__ == '__main__':
+    get_ip_country("172.217.22.78")
