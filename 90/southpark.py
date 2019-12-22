850d01522d6767c23a468946c683a24483fb73ff
Viktor Papara <eucalyptov@gmail.com>
2019-12-22 12:05:58 +0100

    Rewrite implementation with inspiration from official solution
    Bite 90. What South Park characters talk most?

--- 90/southpark.py
+++ 90/southpark.py
@@ -18,16 +18,18 @@ def get_num_words_spoken_by_character_per_episode(content):
     """Receives loaded csv content (str) and returns a dict of
        keys=characters and values=Counter object,
        which is a mapping of episode=>words spoken"""
-    parsed = csv.reader(content.splitlines())
+    parsed = csv.DictReader(content.splitlines())
     words_spoken = defaultdict(Counter)
 
     for row in parsed:
-        person = row[2]
-        words = row[3].split()
-        words_spoken[person].update({row[1]: len(words)})
+        person = row['Character']
+        word_count = len(row['Line'].split())
+        episode = row['Episode']
+        words_spoken[person].update({episode: word_count})
 
     return words_spoken
 
 
 if __name__ == '__main__':
-    get_num_words_spoken_by_character_per_episode(get_season_csv_file(1))
+    words_spoken = get_num_words_spoken_by_character_per_episode(get_season_csv_file(1))
+    print(words_spoken['Agent 1'])
