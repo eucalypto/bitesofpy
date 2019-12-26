7559becb0d9dbe7ee7f5df2e9ad9c22e7039caec
Viktor Papara <eucalyptov@gmail.com>
2019-12-26 20:28:29 +0100

    Implement solution for Bite 116. List and filter files in a directory

--- 116/files.py
+++ 116/files.py
@@ -5,4 +5,10 @@ ONE_KB = 1024
 
 def get_files(dirname, size_in_kb):
     """Return files in dirname that are >= size_in_kb"""
-    pass
+    return (file.name
+            for file in os.scandir(dirname)
+            if file.stat().st_size >= size_in_kb * ONE_KB)
+
+
+if __name__ == '__main__':
+    print(get_files('.', 1))
