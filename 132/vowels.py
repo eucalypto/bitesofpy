cfa68c804e5e129333bcbb02c983b508933615de
Viktor Papara <eucalyptov@gmail.com>
2019-12-29 11:38:50 +0100

    Implement solution for Bite 132. Find the word with the most vowels

--- 132/vowels.py
+++ 132/vowels.py
@@ -5,4 +5,8 @@ def get_word_max_vowels(text):
     """Get the case insensitive word in text that has most vowels.
        Return a tuple of the matching word and the vowel count, e.g.
        ('object-oriented', 6)"""
-    pass
+    def count_vowels(word):
+        return len([True for letter in word if letter in VOWELS])
+    words = [(word, count_vowels(word)) for word in text.split()]
+    return max(words, key=lambda pair: pair[1])
+
