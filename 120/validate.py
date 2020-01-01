44bed5bf156d117092bb0d6b0d7f919b6e0283ff
Viktor Papara <eucalyptov@gmail.com>
2020-01-01 20:08:59 +0100

    Implement solution for Bite 120. Write a numbers validation decorator

--- 120/validate.py
+++ 120/validate.py
@@ -2,5 +2,13 @@ from functools import wraps
 
 
 def int_args(func):
+
     @wraps(func)
-    # complete this decorator
+    def wrapped(*args):
+        if any(type(arg) != int for arg in args):
+            raise TypeError
+        if any(arg < 0 for arg in args):
+            raise ValueError
+        return func(*args)
+
+    return wrapped
