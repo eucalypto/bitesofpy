dbea0c0b4616dd57358b202be1709d00b0c7b61f
Viktor Papara <eucalyptov@gmail.com>
2020-01-02 12:14:28 +0100

    Implement solution for Bite 122. Check if two words are anagrams

--- 122/anagram.py
+++ 122/anagram.py
@@ -2,4 +2,15 @@ def is_anagram(word1, word2):
     """Receives two words and returns True/False (boolean) if word2 is
        an anagram of word1, ignore case and spacing.
        About anagrams: https://en.wikipedia.org/wiki/Anagram"""
-    pass
+    # return true if sorted()
+    def sort_letters(word):
+        return sorted(list(letter.lower() for letter in word if letter.isalnum()))
+
+    if sort_letters(word1) == sort_letters(word2):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(is_anagram('rail safety', 'fairy tales'))
