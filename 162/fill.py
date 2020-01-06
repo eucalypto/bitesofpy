9d23d197ed0545e4291e38f70e71d6c832ae3b6d
Viktor Papara <eucalyptov@gmail.com>
2020-01-06 12:55:16 +0100

    Implement solution for Bite 162. Vertically align output of counters

--- 162/fill.py
+++ 162/fill.py
@@ -3,4 +3,5 @@ HTML_SPACE = '&nbsp;'
 
 def prefill_with_character(value, column_length=4, fill_char=HTML_SPACE):
     """Prepend value with fill_char for given column_length"""
-    pass
+    needed_spaces = column_length - len(str(value))
+    return needed_spaces * fill_char + str(value)
