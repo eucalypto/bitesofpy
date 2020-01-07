213f83d32fd820cbf1924e72e87f362c8f6c90fb
Viktor Papara <eucalyptov@gmail.com>
2020-01-07 19:08:43 +0100

    Implement naive solution for Bite 175. Find missing dates

--- 175/missing_dates.py
+++ 175/missing_dates.py
@@ -1,3 +1,5 @@
+import datetime
+
 def get_missing_dates(dates):
     """Receives a range of dates and returns a sequence
        of missing datetime.date objects (no worries about order).
@@ -7,4 +9,13 @@ def get_missing_dates(dates):
 
        See the Bite description and tests for example outputs.
     """
-    pass
+    start = min(dates)
+    end = max(dates)
+
+    missing = []
+    iteration_day = start
+    while iteration_day < end:
+        if iteration_day not in dates:
+            missing.append(iteration_day)
+        iteration_day += datetime.timedelta(days=1)
+    return missing
