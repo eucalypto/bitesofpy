8c1e4f7342f287c0e3694bf0fd49c5d014b811e9
Viktor Papara <eucalyptov@gmail.com>
2020-01-08 18:01:59 +0100

    Implement solution for Bite 186. Calculate number of books to have read at date ...

--- 186/books.py
+++ 186/books.py
@@ -14,14 +14,18 @@ def get_number_books_read(books_per_year_goal: int,
        If books_per_year_goal negative or 0, or at_date is in the
        past, raise a ValueError."""
     at_date = at_date or str(NOW)
-    # TODOs
 
     # 1. use dateutil's parse to convert at_date into a
     # datetime object
+    at_date = parse(at_date)
 
     # 2. check books_per_year_goal and at_date and raise
     # a ValueError if goal <= 0 or at_date in the past (< NOW)
+    if books_per_year_goal <= 0 or at_date < NOW:
+        raise ValueError
 
     # 3. check the offset of at_date in the year ("week of the
     # year" - e.g. whatweekisit.com) and based on the books_per_year_goal,
     # calculate the number of books that should have been read / completed
+    week_number = at_date.isocalendar()[1]
+    return int(books_per_year_goal / WEEKS_PER_YEAR * week_number)
