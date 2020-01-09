d2e2072ebb6a32b0c1989f84b767ea7fba1c2f87
Viktor Papara <eucalyptov@gmail.com>
2020-01-09 20:26:45 +0100

    Implement solution for Bite 163. Which packages were upgraded?

--- 163/reqs.py
+++ 163/reqs.py
@@ -1,6 +1,23 @@
+def _is_newer_version(new, old):
+    if new == '' or old == '':
+        return False
+    old_version_numbers = old.split('==')[1].split('.')
+    new_version_numbers = new.split('==')[1].split('.')
+    for old_level, new_level in zip(old_version_numbers, new_version_numbers):
+        if int(old_level) < int(new_level):
+            return True
+        elif int(old_level) > int(new_level):
+            return False
+    return False
+
+
 def changed_dependencies(old_reqs: str, new_reqs: str) -> list:
     """Compare old vs new requirement multiline strings
        and return a list of dependencies that have been upgraded
        (have a newer version)
     """
-    pass
+    changed = []
+    for old, new in zip(old_reqs.splitlines(), new_reqs.splitlines()):
+        if _is_newer_version(new, old):
+            changed.append(old.split('==')[0])
+    return changed
