6d7fe0622aa443f02d10b6fa29b4f585884e6bf6
Viktor Papara <eucalyptov@gmail.com>
2020-01-13 00:01:11 +0100

    Implement naive solution for Bite 176: Create a variable length chessboard

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -4,4 +4,13 @@ WHITE, BLACK = ' ', '#'
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    pass
+    for row in range(size):
+        out = ''
+        if row % 2 == 0:
+            for column in range(size):
+                out += WHITE if column % 2 == 0 else BLACK
+        else:
+            for column in range(size):
+                out += WHITE if column % 2 != 0 else BLACK
+        print(out)
+
