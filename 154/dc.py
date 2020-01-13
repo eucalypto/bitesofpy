b844929127c88dee7a555892e9f0f2661bdcf124
Viktor Papara <eucalyptov@gmail.com>
2020-01-13 19:41:33 +0100

    Flesh out dataclass to solve Bite 154: Write your own Data Class

--- 154/dc.py
+++ 154/dc.py
@@ -1,1 +1,17 @@
 from dataclasses import dataclass
+
+
+@dataclass(order=True)
+class Bite:
+    number: int
+    title: str
+    level: str = 'Beginner'
+
+    def __post_init__(self):
+        """
+        Dataclass automatically generates a wonderful __init__() constructor
+        Here, we just want to adjust the constructor by capitalizing the title.
+        We use this hook: __post_init__()
+        :return:
+        """
+        self.title = self.title.capitalize()
