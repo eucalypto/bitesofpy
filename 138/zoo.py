eda2c275dfed00e2a0db3ccedb330d195ceaaad5
Viktor Papara <eucalyptov@gmail.com>
2020-01-15 18:41:02 +0100

    Use itertools.count to improve solution
    Bite 138: OOP fun at the Zoo

--- 138/zoo.py
+++ 138/zoo.py
@@ -1,16 +1,18 @@
+import itertools
+
+
 class Animal:
-    last_animal = 10000
-    all_animals = []
+    _id_count = itertools.count(10001)
+    _all_animals = []
 
     def __init__(self, name):
         self.name = name.capitalize()
-        self.number = Animal.last_animal + 1
-        Animal.last_animal = self.number
-        Animal.all_animals.append(self)
+        self.id = next(Animal._id_count)
+        Animal._all_animals.append(self)
 
     def __str__(self):
-        return f'{self.number}. {self.name}'
+        return f'{self.id}. {self.name}'
 
     @classmethod
     def zoo(cls):
-        return '\n'.join(str(animal) for animal in Animal.all_animals)
+        return '\n'.join(str(animal) for animal in Animal._all_animals)
