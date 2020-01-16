5dfd4359fd2368f36eb9677db0b577fefa847b94
Viktor Papara <eucalyptov@gmail.com>
2020-01-16 12:21:57 +0100

    Use collections.defaultdict to solve Bite 118
    Bite 118. List exercise: return first occurrence indices of duplicated words

--- 118/duplicates.py
+++ 118/duplicates.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def get_duplicate_indices(words):
     """Given a list of words, loop through the words and check for each
        word if it occurs more than once.
@@ -7,4 +10,10 @@ def get_duplicate_indices(words):
        return [0, 1]:
        ['is', 'it', 'true', 'or', 'is', 'it', 'not?'] => [0, 1]
        Make sure the returning list is unique and sorted in ascending order."""
-    pass
+    word_indices = defaultdict(list)
+    for index, word in enumerate(words):
+        word_indices[word].append(index)
+
+    return sorted([occurrences[0]
+                   for occurrences in word_indices.values()
+                   if len(occurrences) > 1])
