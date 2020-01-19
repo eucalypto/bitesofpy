5eeea0c2624ddbdec1ae808921ba2fadd8edce1a
Viktor Papara <eucalyptov@gmail.com>
2020-01-19 14:24:18 +0100

    Use tuple as key in sorted to sort for two properties
    one after the other. This is the official solution from PyBites. ^-^

--- 149/words.py
+++ 149/words.py
@@ -3,10 +3,4 @@ def sort_words_case_insensitively(words):
        (1995, 19ab = numbers / Happy, happy4you = strings, hence for
         numbers you only need to check the first char of the word)
     """
-    starting_with_number = sorted(
-        [word for word in words if word[0].isdigit()],
-        key=str.casefold)
-    not_starting_with_number = sorted(
-        [word for word in words if not word[0].isdigit()],
-        key=str.casefold)
-    return not_starting_with_number + starting_with_number
+    return sorted(words, key=lambda word: (word[0].isdigit(), word.casefold()) )
