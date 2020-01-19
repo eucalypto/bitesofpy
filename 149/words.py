a9ebf04b034e7c54be67f732035352a3ff37792b
Viktor Papara <eucalyptov@gmail.com>
2020-01-19 14:11:40 +0100

    Use list comprehension to solve Bite 149: Sorting words with constraint

--- 149/words.py
+++ 149/words.py
@@ -3,4 +3,10 @@ def sort_words_case_insensitively(words):
        (1995, 19ab = numbers / Happy, happy4you = strings, hence for
         numbers you only need to check the first char of the word)
     """
-    pass
+    starting_with_number = sorted(
+        [word for word in words if word[0].isdigit()],
+        key=str.casefold)
+    not_starting_with_number = sorted(
+        [word for word in words if not word[0].isdigit()],
+        key=str.casefold)
+    return not_starting_with_number + starting_with_number
