8de8148fcf5e9482cf4b6f22c0f725ad88b193d3
Viktor Papara <eucalyptov@gmail.com>
2020-01-20 21:12:49 +0100

    Use os.scandir() to solve Bite 161: Count the number of files and directories

--- 161/tree.py
+++ 161/tree.py
@@ -5,4 +5,18 @@ def count_dirs_and_files(directory='.'):
     """Count the amount of of directories and files in passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    pass
+    files = 0
+    directories = 0
+    for entry in os.scandir(directory):
+        if entry.is_dir():
+            directories += 1
+            more_directories, more_files = count_dirs_and_files(entry)
+            files += more_files
+            directories += more_directories
+        elif entry.is_file():
+            files += 1
+    return directories, files
+
+
+if __name__ == '__main__':
+    print(count_dirs_and_files())
