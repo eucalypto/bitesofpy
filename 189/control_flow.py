bd8af417bb949f0a62da30de6f02fc1d0d1ad817
Viktor Papara <eucalyptov@gmail.com>
2020-01-22 00:31:19 +0100

    Implement solution for Bite 189: Filter a list of names

--- 189/control_flow.py
+++ 189/control_flow.py
@@ -4,4 +4,15 @@ MAX_NAMES = 5
 
 
 def filter_names(names):
-    pass
+    yield_count = 0
+    for name in names:
+        if name.startswith(IGNORE_CHAR):
+            continue
+        if not name.isalpha():
+            continue
+        if name.startswith(QUIT_CHAR):
+            break
+        if yield_count >= MAX_NAMES:
+            break
+        yield_count += 1
+        yield name
