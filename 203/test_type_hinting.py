4911653b4f91cdf750c5da9ca52e23a6734ae4a2
Viktor Papara <eucalyptov@gmail.com>
2020-01-23 06:59:20 +0100

    Add solution for Bite 203: Type hinting practice
    This time, the tests were hidden, so I had to code on the website. After
    passing, the tests were also reveiled.

--- 203/test_type_hinting.py
+++ 203/test_type_hinting.py
@@ -1,1 +1,34 @@
-# tests are hidden for this Bite
+from typing import get_type_hints
+import pytest
+
+from type_hinting import Employee
+
+
+@pytest.fixture(scope="module")
+def mohh():
+    return Employee("Mohhinder", "Suresh", 5, 8, 12.75)
+
+
+@pytest.mark.parametrize("arg, expected", [
+    ("first_name", str),
+    ("last_name", str),
+    ("days_per_week", int),
+    ("hours_per_day", float),
+    ("wage", float),
+])
+def test_employee(mohh, arg, expected):
+    anno = get_type_hints(mohh)
+    assert anno[arg] == expected
+
+
+@pytest.mark.parametrize(
+    "arg, expected", [("number", float), ("places", int),
+                      ("return", str)]
+)
+def test_rounder(mohh, arg, expected):
+    anno = get_type_hints(mohh._rounder)
+    assert anno[arg] == expected
+
+
+def test_weekly_pay(mohh):
+    assert get_type_hints(Employee.weekly_pay.fget)['return'] == str
