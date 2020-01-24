155332fde89ecd7bdda60607eb9f922f8149bbfa
Viktor Papara <eucalyptov@gmail.com>
2020-01-24 15:36:52 +0100

    Use (hashed) dictionary to solve Bite 208: Find the number pairs summing up N

--- 208/combos.py
+++ 208/combos.py
@@ -1,2 +1,9 @@
 def find_number_pairs(numbers, N=10):
-    pass
+    complements = {round(N - number, 2): number for number in numbers}
+    pairs = set()
+    for number in numbers:
+        if number in complements.keys():
+            if number == complements.get(number):
+                continue
+            pairs.add(tuple(sorted((number, complements.get(number)))))
+    return list(pairs)
