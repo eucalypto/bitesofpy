1179837497942fe858674090ed7a683bb00f05eb
Viktor Papara <eucalyptov@gmail.com>
2020-01-25 15:25:44 +0100

    Use unicodedata to solve Bite 157: Filter out accented characters

--- 157/accents.py
+++ 157/accents.py
@@ -1,5 +1,8 @@
+import unicodedata as ud
+
+
 def filter_accents(text):
     """Return a sequence of accented characters found in
        the passed in lowercased text string
     """
-    pass
+    return {char for char in text.lower() if ud.decomposition(char) != ''}
