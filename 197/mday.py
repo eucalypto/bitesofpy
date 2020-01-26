63f9e1305f1654928e992c6ebfe9eca22ef8f1f9
Viktor Papara <eucalyptov@gmail.com>
2020-01-26 19:42:01 +0100

    Use dateutil.relativedata.relativedata to solve
    Bite 197. What date is Mother's Day celebrated?

--- 197/mday.py
+++ 197/mday.py
@@ -1,7 +1,8 @@
 from datetime import date
+from dateutil import relativedelta as rd
 
 
 def get_mothers_day_date(year):
     """Given the passed in year int, return the date Mother's Day
        is celebrated assuming it's the 2nd Sunday of May."""
-    pass
+    return date(year, 5, 1) + rd.relativedelta(weekday=rd.SU(+2))
