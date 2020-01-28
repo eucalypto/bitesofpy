98b43ac4c213c1441c019a8b80c583b7db83926d
Viktor Papara <eucalyptov@gmail.com>
2020-01-28 13:33:06 +0100

    Implement solution for Bite 167. Complete a User class: properties and representation dunder methods

--- 167/user.py
+++ 167/user.py
@@ -14,7 +14,7 @@ class User:
            and using title case for both.
         """
         # TODO 1: you code
-        pass
+        return self.first_name.capitalize() + ' ' + self.last_name.capitalize()
 
     @property
     def username(self):
@@ -26,7 +26,7 @@ class User:
            https://pybit.es/property-decorator.html
         """
         # TODO 2: you code
-        pass
+        return self.first_name[0].lower() + self.last_name[0:7].lower()
 
     # TODO 3: you code
     #
@@ -37,10 +37,10 @@ class User:
     # see also TESTS for required output
 
     def __str__(self):
-        pass
+        return f'{self.get_full_name} ({self.username})'
 
     def __repr__(self):
         """Don't hardcode the class name, hint: use a
            special attribute of self.__class__ ...
         """
-        pass
+        return self.__class__.__name__ + f'("{self.first_name}", "{self.last_name}")'
