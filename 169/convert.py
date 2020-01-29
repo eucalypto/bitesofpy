6a67e0cd5328a98185bb997fef4d5a2a12cc3342
Viktor Papara <eucalyptov@gmail.com>
2020-01-29 20:13:06 +0100

    Implement solution for Bite 169: Simple length converter

--- 169/convert.py
+++ 169/convert.py
@@ -5,4 +5,14 @@ def convert(value: float, fmt: str) -> float:
     :param fmt: String indicating format to convert to
     :return: Float rounded to 4 decimal places after conversion
     """
-    pass
+    if not (type(value) == int or type(value) == float):
+        raise TypeError("Type of the input must be numeric: int or float")
+    if not (fmt.casefold() == 'cm' or fmt.casefold() == 'in'):
+        raise ValueError("Type must be either 'cm' or 'in'")
+
+    cm_per_inch = 2.54
+
+    if fmt.casefold() == 'in':
+        return round(value / cm_per_inch, ndigits=4)
+    elif fmt.casefold() == 'cm':
+        return round(value * cm_per_inch, ndigits=4)
