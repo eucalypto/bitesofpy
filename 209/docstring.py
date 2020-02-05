d6768c152fb0a7c3dcef51f840dd50bdc521eba4
Viktor Papara <eucalyptov@gmail.com>
2020-02-05 18:34:09 +0100

    Implement solution for Bite 209 and add test
    The test was hidden until this bite was solved online.
    Bite 209. Write a Sphinx docstring

--- 209/docstring.py
+++ 209/docstring.py
@@ -1,3 +1,12 @@
 def sum_numbers(numbers):
-    """Write your docstring here ..."""
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+
+    :raises TypeError: if not all numeric values passed in
+
+    :return: sum of numbers
+    :rtype: int
+    """
     pass
