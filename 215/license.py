f3778ac01dd2114a19d0a900fb557d775dff04a0
Viktor Papara <eucalyptov@gmail.com>
2020-02-05 19:20:05 +0100

    Add Sphinx documentation for Bite 215. Validate a license key

--- 215/license.py
+++ 215/license.py
@@ -2,7 +2,13 @@ import re
 
 
 def validate_license(key: str) -> bool:
-    """Write a regex that matches a PyBites license key
+    """Validates license key according to this format: PyBites license key
        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
+
+    :param key: key to be validated
+    :type key: str
+
+    :returns: True if matches, else False
+    :rtype: bool
     """
     return re.match('^PB-[A-Z0-9]{8}-[A-Z0-9]{8}-[A-Z0-9]{8}-[A-Z0-9]{8}$', key) is not None
