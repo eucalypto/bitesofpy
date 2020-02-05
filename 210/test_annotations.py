ccfb17273b68abf20fe95f6baa666e9056a18270
Viktor Papara <eucalyptov@gmail.com>
2020-02-05 18:42:13 +0100

    Implement solution for Bite 210: Add Type Annotations

--- 210/test_annotations.py
+++ 210/test_annotations.py
@@ -1,1 +1,9 @@
-# tests are hidden for this Bite
+import typing
+
+from annotations import sum_numbers
+
+
+def test_sum_numbers():
+    res = sum_numbers.__annotations__
+    assert res.get('numbers') == typing.List[int]
+    assert res.get('return') == int
