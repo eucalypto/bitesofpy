86f2e7d77f1f9a12d05ed8f6d8cf9cfd4bef2793
Viktor Papara <eucalyptov@gmail.com>
2020-02-16 21:45:49 +0100

    Solve Bite 262: GC content

--- 262/gc_content.py
+++ 262/gc_content.py
@@ -3,4 +3,9 @@ def calculate_gc_content(sequence):
     Receives a DNA sequence (A, G, C, or T)
     Returns the percentage of GC content (rounded to the last two digits)
     """
-    pass
+    sequence = list(sequence.upper())
+    counts = {}
+    for base in 'AGCT':
+        counts[base] = sequence.count(base)
+    gc_content = (counts['C'] + counts['G']) / sum(counts.values()) * 100
+    return round(gc_content, ndigits=2)
