937bbf8743148104d1d7731e3bdc5dc2f3b74e33
Viktor Papara <eucalyptov@gmail.com>
2020-02-16 19:11:18 +0100

    Implement solution for Bite 254. Global vs loecal variables

--- 254/scoping.py
+++ 254/scoping.py
@@ -5,4 +5,7 @@ def sum_numbers(numbers: list) -> int:
     """Sums passed in numbers returning the total, also
        update the global variable num_hundreds with the amount
        of times 100 fits in total"""
-    pass
+    summed_up = sum(numbers)
+    global num_hundreds
+    num_hundreds += summed_up // 100
+    return summed_up
