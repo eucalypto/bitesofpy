b897e36cfe1011d7b32890507128fa8c0e2251b3
Viktor Papara <eucalyptov@gmail.com>
2020-02-18 17:11:55 +0100

    Use list comprehension to solve Bite 65: Get all valid dictionary words for a draw of letters

--- 65/scrabble.py
+++ 65/scrabble.py
@@ -19,9 +19,13 @@ def get_possible_dict_words(draw):
     """Get all possible words from a draw (list of letters) which are
        valid dictionary words. Use _get_permutations_draw and provided
        dictionary"""
-    pass
+    return [''.join(permutation).lower()
+            for permutation in _get_permutations_draw(draw)
+            if ''.join(permutation).lower() in dictionary]
+
 
 def _get_permutations_draw(draw):
     """Helper to get all permutations of a draw (list of letters), hint:
        use itertools.permutations (order of letters matters)"""
-    pass
+    for i in range(1, len(draw) + 1):
+        yield from itertools.permutations(draw, i)
